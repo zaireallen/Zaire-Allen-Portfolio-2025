13e023116dffa1eb07308a1dfb0457db23e5cbf1
Zai <allenzaire@gmail.com>
2025-05-14 19:04:54 -0400

    Update Timeline.tsx to adjust text color for improved visibility in the timeline component. This change enhances the user interface by ensuring better contrast and readability of the title text.

--- src/components/ui/Timeline.tsx
+++ src/components/ui/Timeline.tsx
@@ -63,7 +63,7 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
             </div>
 
             <div className="relative pl-20 pr-4 md:pl-4 w-full">
-              <h3 className="md:hidden block text-2xl mb-4 text-left font-bold text-neutral-500 dark:text-neutral-500">
+              <h3 className="md:hidden block text-2xl mb-4 text-left font-bold text-neutral-200 dark:text-neutral-500">
                 {item.title}
               </h3>
               {item.content}{" "}
