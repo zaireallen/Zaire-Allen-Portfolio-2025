1595fd23d152858e0b4a28807822559094985202
Zai <allenzaire@gmail.com>
2025-05-14 20:33:05 -0400

    improved the header section text. link preview works somewhat

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -5,19 +5,21 @@ export function Header() {
     return (
         <>
         <div className="max-w-7xl pt-40 px-4 md:px-8 lg:px-10 z-40">
-            <h1 className="font-medium text-xl md:text-4xl mb-2 text-black dark:text-white max-w-xl">
+            <h1 className="text-2xl tracking-normal font-extralight mb-2 text-black dark:text-slate-300 max-w-xl">
                 Zaire Allen
             </h1>
             <div className="flex flex-col gap-2">
-                <p className="text-slate-500 dark:text-slate-400 text-base md:text-lg max-w-4xl">
-                    Hey, I'm Zaire. A growth-minded design engineer.
+                <p className="text-slate-500 dark:text-slate-100 font-light text-3xl max-w-4xl pb-4">
+                    Hey, I'm a New York based growth-driven design engineer.
+                    I build and like to market too.
                 </p>
-                <p className="text-slate-500 dark:text-slate-400 text-base md:text-lg max-w-4xl">
-                    Currently working on <LinkPreview url="https://www.figma.com">Figma</LinkPreview>.
-                </p>
-                
-                <p className="text-slate-500 dark:text-slate-400 text-base md:text-lg max-w-4xl">
-                    Currently working on <LinkPreview url="https://www.figma.com">Figma</LinkPreview>.
+
+                <p className="text-slate-200 dark:text-slate-100 font-light text-3xl max-w-4xl py-4">
+                    Currently working on
+                    <LinkPreview 
+                    url="https://www.waypointdaily.com" 
+                    imageSrc="src/assets/WaypointMeta.webp" 
+                    isStatic>Waypoint</LinkPreview>.
                 </p>
 
             </div>
