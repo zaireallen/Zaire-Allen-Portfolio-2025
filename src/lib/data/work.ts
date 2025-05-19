a48abcbd57755016da38c7983030f0bbc68ca5d3
Zai <allenzaire@gmail.com>
2025-05-19 17:41:15 -0400

    updated file sizes

--- src/lib/data/work.ts
+++ src/lib/data/work.ts
@@ -31,7 +31,7 @@ export type DisplayWork = {
       id: 1,
       title: "Love Circular Learning Platform Mobile App",
       images: [
-        { src: "/assets/work/LC/Love Circular - Learning Platform - Mobile display.webp", objectPosition: "0% 0%" },
+        { src: "/assets/work/LC/Love Circular - Learning Platform - Mobile display.webp", objectPosition: "50% 0%" },
         { src: "/assets/work/LC/Love Circular - Learning Platform - Mobile screens.webp", objectPosition: "0% 0%" }
       ],
       subtitle: "We were on a Times Square Billboard",
@@ -102,7 +102,21 @@ export type DisplayWork = {
       title: "Waypoint",
       images: [
         { src: "/assets/work/Waypoint/Waypoint Web App2.gif", objectPosition: "0% 70%" },
-        { src: "/assets/work/Waypoint/Waypoint - Marketing Site - Desktop 1.webp", objectPosition: "0% 0%" }
+        { src: "/assets/work/Waypoint/Waypoint - App - Desktop 1.webp", objectPosition: "0% 0%" }
+      ],
+      subtitle: "We were on a Times Square Billboard",
+      content: [
+        "We hit Times Square after winning Whop's $3k in 30 dayscompetition on a billboard.",
+        "Back to teaching. Couldn't resist—now part-time at  \"96mins.\" helping designers level up their skills (and incomes).",
+        "Writing code in my spare time, hunting for the last Infinity Stone by shipping my own designs.",
+      ]
+    },
+    {
+      id: 7,
+      title: "Unnamed Mobile App",
+      images: [
+        { src: "/assets/work/GK/GK-mockups.webp", objectPosition: "0% 0%" },
+        { src: "/assets/work/GK/GK.webp", objectPosition: "0% 0%" }
       ],
       subtitle: "We were on a Times Square Billboard",
       content: [
