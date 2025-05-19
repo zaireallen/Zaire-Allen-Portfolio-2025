cadadb6fe4b713d6db10c3eb2d4acf20dfeb906c
Zai <allenzaire@gmail.com>
2025-05-19 17:44:27 -0400

    removed the bloated file

--- src/lib/data/work.ts
+++ src/lib/data/work.ts
@@ -1,22 +1,3 @@
-// Waypoint
-// import WaypointDesktop1 from '/src/assets/work/Waypoint/Waypoint - App - Desktop 1.webp'
-// import WaypointDesktop2 from '/src/assets/work/Waypoint/Waypoint - Marketing Site - Desktop 1.webp'
-
-// RullowJacobs
-// import RullowJacobsDesktop1 from '/src/assets/work/RullowJacobs/Rullow Jacobs-Desktop-1.webp'
-// import RullowJacobsDesktop2 from '/src/assets/work/RullowJacobs/Rullow Jacobs-Desktop-2.webp'
-
-// SitesExplained
-// import SitesExplainedDesktop1 from '/src/assets/work/SitesExplained/sitesexplained-desktop-1.webp'
-// import SitesExplainedDesktop2 from '/src/assets/work/SitesExplained/sitesexplained-desktop-2.webp'
-//import SitesExplainedDesktop3 from '/src/assets/work/SitesExplained/sitesexplained-desktop-3.webp'
-// import SitesExplainedDesktop4 from '/src/assets/work/SitesExplained/sitesexplained-desktop-4.webp'
-
-// Love Circular
-// import LearningPlatform from '/src/assets/work/LC/Love Circular - Learning Platform - Mobile display.webp'
-// import LearningPlatformSecond from '/src/assets/work/LC/Love Circular - Learning Platform - Mobile screens.webp'
-
-
 // Type definition for timeline entries
 export type DisplayWork = {
     id: number;
@@ -41,20 +22,20 @@ export type DisplayWork = {
         "Writing code in my spare time, hunting for the last Infinity Stone by shipping my own designs.",
       ]
     },
-    {
-      id: 2,
-      title: "Love Circular Year in Review 2023",
-      images: [
-        { src: "/assets/work/LC/LC 2023 in Review Site.gif", objectPosition: "0% 0%" },
-        { src: "/assets/work/LC/LC In Review 2023 Hire Count.webp", objectPosition: "0% 0%" }
-      ],
-      subtitle: "We were on a Times Square Billboard",
-      content: [
-        "We hit Times Square after winning Whop's $3k in 30 dayscompetition on a billboard.",
-        "Back to teaching. Couldn't resist—now part-time at  \"96mins.\" helping designers level up their skills (and incomes).",
-        "Writing code in my spare time, hunting for the last Infinity Stone by shipping my own designs.",
-      ]
-    },
+    // {
+    //   id: 2,
+    //   title: "Love Circular Year in Review 2023",
+    //   images: [
+    //     { src: "/assets/work/LC/LC 2023 in Review Site.gif", objectPosition: "0% 0%" },
+    //     { src: "/assets/work/LC/LC In Review 2023 Hire Count.webp", objectPosition: "0% 0%" }
+    //   ],
+    //   subtitle: "We were on a Times Square Billboard",
+    //   content: [
+    //     "We hit Times Square after winning Whop's $3k in 30 dayscompetition on a billboard.",
+    //     "Back to teaching. Couldn't resist—now part-time at  \"96mins.\" helping designers level up their skills (and incomes).",
+    //     "Writing code in my spare time, hunting for the last Infinity Stone by shipping my own designs.",
+    //   ]
+    // },
     {
       id: 3,
       title: "Rullow Jacobs",
