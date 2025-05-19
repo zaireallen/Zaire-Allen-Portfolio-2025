c7f76d27c9a8e5f884cc10cba899d6ce5ad490c3
Zai <allenzaire@gmail.com>
2025-05-18 23:13:24 -0400

    push to website

--- src/lib/data/work.ts
+++ src/lib/data/work.ts
@@ -1,6 +1,11 @@
 import LearningPlatform from '/src/assets/work/LC/Love Circular - Learning Platform - Mobile.webp'
 import LearningPlatformSecond from '/src/assets/work/LC/Love Circular - Learning Platform - Mobile screens.webp'
-import SitesExplained1 from ''
+import SitesExplainedDesktop1 from '/src/assets/work/LC/sitesexplained-desktop-1.webp'
+// import SitesExplainedDesktop2 from '/src/assets/work/LC/sitesexplained-desktop-2.webp'
+// import SitesExplainedDesktop3 from '/src/assets/work/LC/sitesexplained-desktop-3.webp'
+import SitesExplainedDesktop4 from '/src/assets/work/LC/sitesexplained-desktop-4.webp'
+
+
 
 // Type definition for timeline entries
 export type DisplayWork = {
@@ -14,6 +19,20 @@ export type DisplayWork = {
   export const workData: DisplayWork[] = [
     {
       id: 1,
+      title: "Love Circular Learning Platform Mobile App",
+      images: [
+        { src: SitesExplainedDesktop1, objectPosition: "0% 0%" },
+        { src: SitesExplainedDesktop4, objectPosition: "0% 0%" }
+      ],
+      subtitle: "We were on a Times Square Billboard",
+      content: [
+        "We hit Times Square after winning Whop’s $3k in 30 dayscompetition on a billboard.",
+        "Back to teaching. Couldn’t resist—now part-time at  \"96mins.\" helping designers level up their skills (and incomes).",
+        "Writing code in my spare time, hunting for the last Infinity Stone by shipping my own designs.",
+  
+      ]
+    },{
+      id: 2,
       title: "Love Circular Learning Platform Mobile App",
       images: [
         { src: LearningPlatform, objectPosition: "0% 0%" },
